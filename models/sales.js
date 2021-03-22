b465bcf9baa417cefd824919e99ef685d5a33d13
dev-dy <dheerajkryadav08@gmail.com>
2021-03-22 10:46:45 +0530

    'schema'

---
+++ models/sales.js
@@ -0,0 +1,52 @@
+const Sequelize = require('sequelize');
+const db = require('../config/postgres');
+
+const Gig = db.define('Sales', {
+  Region: {
+    type: Sequelize.STRING
+  },
+  Country: {
+    type: Sequelize.STRING
+  },
+  itemTypes: {
+    type: Sequelize.STRING
+  },
+  salesChannel: {
+    type: Sequelize.STRING
+  },
+  orderPrice: {
+    type: Sequelize.STRING
+  },
+  orderDate: {
+    type: Sequelize.DATE
+  },
+  orderId: {
+    type: Sequelize.INTEGER
+  },
+  shipDate: {
+    type: Sequelize.DATE
+  },
+  unitsSold: {   
+    type: Sequelize.INTEGER
+  },
+  unitPrice: {
+    type: Sequelize.INTEGER
+  },
+  unitCost: {
+    type: Sequelize.INTEGER
+  },
+  totalRevenue: {
+    type: Sequelize.INTEGER
+  },
+  totalCost: {
+    type: Sequelize.INTEGER
+  },
+  totalProfit: {
+    type: Sequelize.INTEGER
+  }
+});
+
+Sales.sync().then(() => {
+  console.log('table created');
+});
+module.exports = Sales;
